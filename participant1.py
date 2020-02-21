cab69a59fd4e3b84d67d16b83c96556360b29989
Sam Smith <sjs5pg@virginia.edu>
2020-02-21 12:32:03 -0500

    This is the basic file reading script that I have written thus far.

---
+++ participant1.py
@@ -0,0 +1,44 @@
+#This file reads in data from one of the sample files
+#that Shannon sent and and multiplies the x and y positions of the left eye
+#by the correct dimensions.
+
+x = open("practicedata1.csv", 'r')
+lines = x.readlines()
+
+#Finding the data we need in the csv 
+columns = lines[0]
+colLst = columns.split(',')
+leftpogy = 0
+leftpogx = 0
+i = 0
+for col in colLst:
+    if col=="LeftPogX":
+        leftpogx = i
+    if col=="LeftPogY":
+        leftpogy = i
+    i+=1
+
+
+l = lines[2].split(',')
+# print(l[24])
+# print(leftpogx)
+
+counter = 0
+
+#First two lines do not have floating point values in left eye positions 
+#Only going to 50000, python doing unexpected things when intputting full list 
+for i in range(2, 50000):
+    cells = lines[i].split(',')
+
+    x = float((cells[leftpogx]))*2560
+
+    y = float((cells[leftpogy]))*1440
+
+    strx = str(x)
+    stry = str(y)
+    
+    #print(strx)
+
+
+
+
