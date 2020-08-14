d223569f65bc8a96787ed46b4c26bc2c93995533
jadatweh <66697390+jadatweh@users.noreply.github.com>
2020-08-14 22:00:22 +0300

    Update ED_with_pandas.py

--- ED_with_pandas.py
+++ ED_with_pandas.py
@@ -122,8 +122,8 @@ def event_detection(tracker_type):
         if flag==False:
             #999 for both velocities and times?
             onset_indices.append(left_most)
-            onsets_times.append(999)
-            onsets_velocities.append(999)
+            onsets_times.append(999999)
+            onsets_velocities.append(999999)
         left_most=i
 
     df["OnsetsTimes"] = pd.Series(onsets_times)
