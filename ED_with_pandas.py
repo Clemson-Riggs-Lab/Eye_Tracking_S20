91873451de5d4e038c85a7bde74432a736def015
Sam Smith <sjs5pg@virginia.edu>
2020-08-17 11:24:40 -0400

    Added testing and fixation radius to GUI

--- ED_with_pandas.py
+++ ED_with_pandas.py
@@ -35,8 +35,8 @@ That's why fixations are short as not my purpose now
 # velocities = [60,54,50,37,30,45,80,100,114,137,150,142,133,128,117,90,75,60,45,55,33,36,37,36,45,60,75,80,105,140,165,140,127,120,95,90,75,65,50,75,35,36,37,36,37,45,60,80,96,110,140,160,145,140,125,100,88,70,55,43,30,41,55,60,39,36,38,35,60,77,90,110,135,145,120,105,90,71,60,50,40,33,37,43,55]
 
 def event_detection(tracker_type):
-    # file_name = inputET_name.get()
-    file_name="Ready_for_ED.csv"
+    file_name = inputET_name.get()
+    # Leaving for testing: file_name="Ready_for_ED.csv"
     df = pd.read_csv(file_name)
     if tracker_type == 1:
         # counter=0
@@ -60,14 +60,11 @@ def event_detection(tracker_type):
         time = list(time)
         velocities = list(velocities)
 
-    #commenting these out for testing purposes
-    # PT = int(peak_thresh.get())
-    # onset_threshold = int(on_thresh.get())
-    # offset_threshold = int(off_thresh.get())
+    PT = int(peak_thresh.get())
+    onset_threshold = int(on_thresh.get())
+    offset_threshold = int(off_thresh.get())
 
-    PT = 279   
-    onset_threshold = 157
-    offset_threshold = 172
+  
 
     #I chose any numbers until we determine the velocity threshold, onset and offset thresholds
     #arbitrary values, PT will be determined before (each participant has a different PT for a different load)
@@ -121,7 +118,7 @@ def event_detection(tracker_type):
                     break
         if flag==False:
             #999 for both velocities and times?
-            onset_indices.append(left_most)
+            onset_indices.append(i-1)
             onsets_times.append(999)
             onsets_velocities.append(999)
         left_most=i
@@ -150,7 +147,7 @@ def event_detection(tracker_type):
                 flag=True
                 break
         if flag==False:
-            offset_indices.append(right_most)
+            offset_indices.append(i+1)
             offsets_times.append(0)
             offsets_velocities.append(0)
 
@@ -192,7 +189,7 @@ def event_detection(tracker_type):
                     dist = calculateDistance(df.at[g, "BestPogX"], df.at[g, "BestPogY"] ,
                     df.at[index, "BestPogX"], df.at[index, "BestPogY"])
                             #this is the pixel radius. Not sure if this value is supposed to be data driven
-                    if dist <= 500:
+                    if dist <= int(fix_radius.get()):
                         fix_lst.append(index)
                     else:
                         break
@@ -227,78 +224,93 @@ def event_detection(tracker_type):
         sac_amps.append(sac_amp)
     
     df["SaccadeAmplitudes"] = pd.Series(sac_amps)
+    
 
     """
     updating the csv with the new information
     """
-    # out_file = output_name.get()
-    out_file = "output_ED.csv"
+    out_file = output_name.get()
+    # out_file = "output_ED.csv"
     df.to_csv(out_file, index=False)
     
-    counter=0
+
+    #For testing 
+    """
+    This nested for loop checks if there is overlap between saccade indices
+    and fixation indices. There was no overlap on my end which is a good sign. 
+    Also BEWARE, this testing loop requires a lot of computing power so make sure
+    you don't have a lot of other things open when running it!
+    """
     for i in fixations_after_constraints:
         for j in i:
-            counter+=1
-
-    print(counter)
+            for s in saccade_indices:
+                if s == j:
+                    print("Overlap")
+              
+    
+    #This is our total number of fixations
     print(len(fixations_after_constraints))
   
-event_detection(2)
 # UI things. This is very similar to preprocessing UI 
-# window = Tk()
-# frame0 = Frame(window)
-# frame1 = Frame(window)
-# frame2 = Frame(window)
-# frame3 = Frame(window)
-# frame4 = Frame(window)
-# frame5 = Frame(window)
-# frame6 = Frame(window)
-# frame7 = Frame(window)
+window = Tk()
+frame0 = Frame(window)
+frame1 = Frame(window)
+frame2 = Frame(window)
+frame3 = Frame(window)
+frame4 = Frame(window)
+frame5 = Frame(window)
+frame6 = Frame(window)
+frame7 = Frame(window)
 
-# frame0.pack()
-# frame1.pack()
-# frame2.pack()
-# frame3.pack()
-# frame4.pack()
-# frame5.pack()
-# frame6.pack()
-# frame7.pack()
+frame0.pack()
+frame1.pack()
+frame2.pack()
+frame3.pack()
+frame4.pack()
+frame5.pack()
+frame6.pack()
+frame7.pack()
 
-# window.title("Event Detection GUI")
+window.title("Event Detection GUI")
 
-# text0 = Label(frame0, text='Enter Eyetracking file name: ')
-# text0.pack(side=LEFT)
-# inputET_name = Entry(frame0)
-# inputET_name.pack(side=LEFT)
+text0 = Label(frame0, text='Enter Eyetracking file name: ')
+text0.pack(side=LEFT)
+inputET_name = Entry(frame0)
+inputET_name.pack(side=LEFT)
 
-# text1 = Label(frame1, text='Enter Output file name: ')
-# text1.pack(side=LEFT)
-# output_name = Entry(frame1)
-# output_name.pack(side=LEFT)
+text1 = Label(frame1, text='Enter Output file name: ')
+text1.pack(side=LEFT)
+output_name = Entry(frame1)
+output_name.pack(side=LEFT)
 
-# text2 = Label(frame2, text='Enter Peak Threshold: ')
-# text2.pack(side=LEFT)
-# peak_thresh = (Entry(frame2))
-# peak_thresh.pack(side=LEFT)
+text2 = Label(frame2, text='Enter Peak Threshold: ')
+text2.pack(side=LEFT)
+peak_thresh = (Entry(frame2))
+peak_thresh.pack(side=LEFT)
 
-# text3 = Label(frame3, text='Enter Onset Threshold: ')
-# text3.pack(side=LEFT)
-# on_thresh = Entry(frame3)
-# on_thresh.pack(side=LEFT)
+text3 = Label(frame3, text='Enter Onset Threshold: ')
+text3.pack(side=LEFT)
+on_thresh = Entry(frame3)
+on_thresh.pack(side=LEFT)
 
-# text4 = Label(frame4, text='Enter Offset Threshold: ')
-# text4.pack(side=LEFT)
-# off_thresh = Entry(frame4)
-# off_thresh.pack(side=LEFT)
+text4 = Label(frame4, text='Enter Offset Threshold: ')
+text4.pack(side=LEFT)
+off_thresh = Entry(frame4)
+off_thresh.pack(side=LEFT)
 
-# one = Button(window, text="Gazepoint", width="10", height="3",command=lambda : event_detection(1))
-# one.pack(side="top")
+text5 = Label(frame5, text='Enter Fixation Radius (px): ')
+text5.pack(side=LEFT)
+fix_radius = Entry(frame5)
+fix_radius.pack(side=LEFT)
 
-# two = Button(window, text="FOVIO", width="10", height="3", command=lambda : event_detection(2))
-# two.pack(side="top")
+one = Button(window, text="Gazepoint", width="10", height="3",command=lambda : event_detection(1))
+one.pack(side="top")
 
-# text6 = Label(frame7, text='Status: N/A')
-# text6.pack()
+two = Button(window, text="FOVIO", width="10", height="3", command=lambda : event_detection(2))
+two.pack(side="top")
 
-# window.mainloop()
+text6 = Label(frame7, text='Status: N/A')
+text6.pack()
+
+window.mainloop()
 
